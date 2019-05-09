b4e9b539c26c77fcd2c5d9c61efdd0e3ec5ad940
zenjava <138962816@qq.com>
2019-05-09 17:11:12 +0800

    修改；   * 发布版本。

--- tasks/publish.sh
+++ tasks/publish.sh
@@ -10,15 +10,15 @@ lerna="$root_path/node_modules/.bin/lerna"
 
 cd "$root_path/packages/devtools-core"
 $lugiac build
-echo 'build lugiax-core'
+echo 'build devtools-core'
 
 cd "$root_path/packages/devtools-conversion"
 $lugiac build
-echo 'build lugiax-router'
+echo 'build devtools-conversion'
 
 cd "$root_path/packages/devtools-material"
 $lugiac build
-echo 'build lugiax'
+echo 'build devtools-material'
 
 cd "$root_path"
 $lerna publish "$@/target"
