cee1507543a19c8b2e8df4ee3e6c59ae3b4bd111
grguang <945455382@qq.com>
2019-09-09 15:46:23 +0800

    Update:     * example;

--- packages/devtools-example/src/pages/Page.js
+++ packages/devtools-example/src/pages/Page.js
@@ -1,41 +1,42 @@
-import React from 'react';
-import styled from 'styled-components';
-import { Theme } from '@lugia/lugia-web';
-import { Button } from '@lugia/lugia-web';
-import { themeHandle } from '@lugia/lugia-web/dist/common/lugiadCore';
-import hello from '../models/hello';
+import React from "react";
+import styled from "styled-components";
+import { Theme } from "@lugia/lugia-web";
+import { Button } from "@lugia/lugia-web";
+import { LugiadCore } from "@lugia/lugia-web";
+import hello from "../models/hello";
+const { themeHandle } = LugiadCore;
 export default class Page extends React.Component {
   componentDidMount() {
-    hello.mutations.load({ eventName: 'componentDidMount' });
+    hello.mutations.load({ eventName: "componentDidMount" });
   }
   componentWillUnmount() {
-    hello.mutations.load({ eventName: 'componentWillUnmount' });
+    hello.mutations.load({ eventName: "componentWillUnmount" });
   }
   render() {
     return (
       <div
         style={{
-          width: '1920px',
-          height: '1080px',
-          zIndex: '4000',
-          position: 'relative',
+          width: "1920px",
+          height: "1080px",
+          zIndex: "4000",
+          position: "relative"
         }}
       >
         <div
           style={{
-            position: 'absolute',
-            width: 38 + 'px',
-            height: 35 + 'px',
-            zIndex: '0',
-            left: 603 + 'px',
-            top: 448 + 'px',
+            position: "absolute",
+            width: 38 + "px",
+            height: 35 + "px",
+            zIndex: "0",
+            left: 603 + "px",
+            top: 448 + "px"
           }}
         >
           <Theme
             config={{
-              wbZbwA72: themeHandle('wbZbwA72', undefined, {
-                Container: { normal: { background: { color: 'yellow' } } },
-              }),
+              wbZbwA72: themeHandle("wbZbwA72", undefined, {
+                Container: { normal: { background: { color: "yellow" } } }
+              })
             }}
           >
             <Button viewClass="wbZbwA72" />
