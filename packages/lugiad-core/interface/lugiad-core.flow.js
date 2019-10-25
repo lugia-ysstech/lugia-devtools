23359e2ede5e9f934b62a1bc7b1fc056580f8f07
grguang <945455382@qq.com>
2019-10-25 21:17:42 +0800

    Add:     * lugiad-core package;

---
+++ packages/lugiad-core/interface/lugiad-core.flow.js
@@ -0,0 +1,21 @@
+/**
+ * Created by liguoxin on 2017/3/14.
+ * @flow
+ */
+declare module '@lugia/devtools-conversion' {
+  declare type PointType =
+    | "leftTop"
+    | "leftBottom"
+    | "rightTop"
+    | "rightBottom";
+  declare type Point = [number, number];
+
+  declare type LugiaDCoreType = {
+    bindHandleEvent: Function,
+    themeHandle: Function,
+    getData: Function,
+    pointType2GetCSS: Object
+  };
+
+  declare module.exports: LugiaDCoreType;
+}
