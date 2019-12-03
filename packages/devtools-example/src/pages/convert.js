e21147e134a5c6036d39c2f165e5cf2909b3fd99
grguang <945455382@qq.com>
2019-12-03 11:49:42 +0800

    Update:     * example;

--- packages/devtools-example/src/pages/convert.js
+++ packages/devtools-example/src/pages/convert.js
@@ -8,4 +8,4 @@ const Convert = require('../../../devtools-conversion/lib');
 const Test = require('./test.json');
 const fs = require('fs');
 
-fs.writeFileSync(__dirname + '/Page.js', Convert(Test));
+fs.writeFileSync(__dirname + '/Page.js', Convert(Test, { exportCusCmp: true }));
