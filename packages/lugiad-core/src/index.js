1ded7e5b2f23f56b828124d8f6d1cb5144b69a89
zenjava <138962816@qq.com>
2020-03-04 22:12:43 +0800

    修正BUG：    * top 和 bottom默认都不能用百分比，以为我们现在的智能适配其实只针对宽度。

--- packages/lugiad-core/src/index.js
+++ packages/lugiad-core/src/index.js
@@ -4,7 +4,7 @@
  *
  * @flow
  */
-import type { PointType, Point } from '@lugia/lugiad-core';
+import type { PointType, Point } from "@lugia/lugiad-core";
 
 function bindHandleEvent(e: Object): string {
   if (!e) {
@@ -39,7 +39,7 @@ function themeHandle(
   }
 
   if (useSmart && cmpTheme.Container && cmpTheme.Container.normal) {
-    cmpTheme.Container.normal.width = '100%';
+    cmpTheme.Container.normal.width = "100%";
   }
   return cmpTheme || {};
 }
@@ -53,46 +53,94 @@ function getData(
   if (!modelName) {
     return {};
   }
-  const paths = fieldName.split('.');
+  const paths = fieldName.split(".");
   const data = modelName.getState().getIn(paths);
   return {
     [propsName]:
-      typeof data !== 'object'
+      typeof data !== "object"
         ? data
         : data
-          ? data.toJS
-            ? data.toJS()
-            : data
-          : null,
+        ? data.toJS
+          ? data.toJS()
+          : data
+        : null
   };
 }
 
+const getSymbol = (pointFix: boolean) => (pointFix ? "px" : "%");
+const getPoint = (point, percentPoint, pointFix) =>
+  pointFix ? point : percentPoint;
+
 const pointType2GetCSS: {
-  [key: PointType]: (point: Point, symbol: ?string) => Object
+  [key: PointType]: (
+    pxPoint: Point,
+    percentPoint: Point,
+    pointFix: boolean
+  ) => Object
 } = {
-  leftTop: (point: Point, symbol: ?string = '%'): Object => {
-    const [ x, y ] = point;
-    return { left: `${x}${symbol}`, top: `${y}${symbol}`, right: '', bottom: '' };
+  leftTop: (pxPoint: Point, percentPoint: Point, pointFix: boolean): Object => {
+    const point = getPoint(pxPoint, percentPoint, pointFix);
+    const [x, y] = point;
+    const symbol = getSymbol(pointFix);
+    return {
+      left: `${x}${symbol}`,
+      top: `${pxPoint[1]}px`,
+      right: "",
+      bottom: ""
+    };
   },
-  leftBottom: (point: Point, symbol: ?string = '%'): Object => {
-    const [ x, y ] = point;
-    return { left: `${x}${symbol}`, bottom: `${y}${symbol}`, right: '', top: '' };
+  leftBottom: (
+    pxPoint: Point,
+    percentPoint: Point,
+    pointFix: boolean
+  ): Object => {
+    const point = getPoint(pxPoint, percentPoint, pointFix);
+    const [x, y] = point;
+    const symbol = getSymbol(pointFix);
+    return {
+      left: `${x}${symbol}`,
+      bottom: `${pxPoint[1]}px`,
+      right: "",
+      top: ""
+    };
   },
-  rightTop: (point: Point, symbol: ?string = '%'): Object => {
-    const [ x, y ] = point;
-    return { right: `${x}${symbol}`, top: `${y}${symbol}`, left: '', bottom: '' };
+  rightTop: (
+    pxPoint: Point,
+    percentPoint: Point,
+    pointFix: boolean
+  ): Object => {
+    const point = getPoint(pxPoint, percentPoint, pointFix);
+    const [x, y] = point;
+    const symbol = getSymbol(pointFix);
+    return {
+      right: `${x}${symbol}`,
+      top: `${pxPoint[1]}px`,
+      left: "",
+      bottom: ""
+    };
   },
-  rightBottom: (point: Point, symbol: ?string = '%'): Object => {
-    const [ x, y ] = point;
-    return { right: `${x}${symbol}`, bottom: `${y}${symbol}`, left: '', top: '' };
-  },
+  rightBottom: (
+    pxPoint: Point,
+    percentPoint: Point,
+    pointFix: boolean
+  ): Object => {
+    const point = getPoint(pxPoint, percentPoint, pointFix);
+    const [x, y] = point;
+    const symbol = getSymbol(pointFix);
+    return {
+      right: `${x}${symbol}`,
+      bottom: `${pxPoint[1]}px`,
+      left: "",
+      top: ""
+    };
+  }
 };
 
 const lugiaDCore = {
   bindHandleEvent,
   themeHandle,
   getData,
-  pointType2GetCSS,
+  pointType2GetCSS
 };
 
 export default lugiaDCore;
