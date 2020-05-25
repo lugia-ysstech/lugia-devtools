6515d3c822baa634610f683131f0b85c0159fd43
grguang <945455382@qq.com>
2020-05-25 15:53:19 +0800

    修改:     * 导出主题元信息方法可返回字符串;

--- packages/devtools-material/interface/devtools-material.flow.js
+++ packages/devtools-material/interface/devtools-material.flow.js
@@ -3,25 +3,27 @@
  * @flow
  */
 declare module '@lugia/devtools-material' {
-  declare type ExtendParam = {
-    outExtend: string,
-    max: number,
+  declare type OutFileType = {
     outFile: string
   };
+  declare type ExtendParam = OutFileType & {
+    outExtend: string,
+    max: number
+  };
+
   declare type InvalidType = string[];
+  declare type ThemeMetaParams = {
+    targetPath: string,
+    invalid: InvalidType,
+    option: OutFileType
+  };
   declare type DesignInfo = {
     createDesignInfo(
       targetPath: string,
       invalid: InvalidType,
       opt: ExtendParam
     ): string,
-    createThemeMeta(targetPath: string, invalid: InvalidType): void
-  };
-
-  declare type ThemeMetaParams = {
-    targetPath: string,
-    invalid: InvalidType,
-    opt: Object
+    createThemeMeta(params: ThemeMetaParams): void
   };
 
   declare module.exports: DesignInfo;
