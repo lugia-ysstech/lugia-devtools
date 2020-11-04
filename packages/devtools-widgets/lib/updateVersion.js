ab0826b6180c14dbbd71b21f2ec3a45d2f0f42ac
liyaqin <liyaqin@ysstech.com>
2020-11-04 15:28:52 +0800

    add： 新增自动增加版本的脚本文件

---
+++ packages/devtools-widgets/lib/updateVersion.js
@@ -0,0 +1,55 @@
+/**
+ *
+ * @flow
+ */
+
+const { writeJSON, readJson } = require('fs-extra');
+
+declare type paramType = {
+  forceAlpha: boolean,
+  filePath: string
+}
+
+async function changeVersion(param: paramType) {
+  console.info('--- 检测版本 ---');
+  const { filePath } = param;
+  try {
+    const packageObj = await readJson(filePath);
+    const currentVersion = packageObj.version;
+    const { forceAlpha } = param;
+    const newVersion = getNewVersion(currentVersion, forceAlpha);
+    packageObj.version = newVersion;
+    console.info('--- 写入新版本 ---：', newVersion, '当前版本', currentVersion);
+    try {
+      await writeJSON(filePath, packageObj);
+      console.log('--- 版本修改成功! ---');
+    } catch (err) {
+      console.error(err);
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+function getNewVersion(version: string, forceAlpha: boolean): string {
+  let newVersion = '1.0.0-alpha.0';
+  if (!version) {
+    return newVersion;
+  }
+
+  const filterResult = version.match(/\.\d+$/g);
+  if (filterResult) {
+    const filterVersion = filterResult[0];
+    if (filterVersion) {
+      const currentVersion = Number(filterVersion.replace(/\./g, ''));
+      const newVersionNumber = currentVersion + 1;
+      const matchAlpha = version.match(/alpha/g);
+      const isAlpha = forceAlpha ? matchAlpha && matchAlpha.length > 0 : true;
+      newVersion = isAlpha ? version.replace(/\.\d+$/g, `.${newVersionNumber}`) : `${version}-alpha.0`;
+    }
+  }
+
+  return newVersion;
+}
+
+module.exports = changeVersion;
